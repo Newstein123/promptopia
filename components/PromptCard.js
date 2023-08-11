f89876c1dbeadca2e308172fde9c64fdfcc34092
Newstein123 <102919096+Newstein123@users.noreply.github.com>
2023-08-11 23:08:47 +0630

    new commit

--- components/PromptCard.js
+++ components/PromptCard.js
@@ -14,11 +14,11 @@ const PromptCard = ({ post, handleEdit, handleDelete, handleTagClick }) => {
 
   const handleProfileClick = () => {
 
-    // if (post.creator._id === session?.user.id) {
-    //   router.push("/profile");
-    // }
+    if (post.creator._id === session?.user.id) {
+      router.push("/profile");
+    }
 
-    // router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
+    router.push(`/profile/${post.creator._id}?name=${post.creator.username}`);
   };
 
   const handleCopy = () => {
